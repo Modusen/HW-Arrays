d1be289c7e156a1b7596d1b38c1ac42d339f61f6
astra <diman.lan@Mail.ru>
2022-07-21 21:22:06 +0300

    Done somehow :)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,85 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+//1st + 2nd task
+        System.out.println("1 задача и 2 задача");
+
+        int [] arr = new int[3];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        for (int i = 0; i < arr.length; i++) {
+            if (i < arr.length-1)
+            System.out.print(arr[i] + ", ");
+            else
+                System.out.println(arr[i]);
+        }
+        double [] doubles = {1.57, 7.654, 9.986};
+            System.out.println(Arrays.toString(doubles));
+
+
+        int[] ints = {1, 2, 3, 15, 12, 54, 664};
+        for (int i = 0; i < ints.length - 1; i++) {
+            System.out.print(ints[i] + ", ");
+        }
+        System.out.println(ints[ints.length - 1]);
+
+System.out.println("А теперь в обратном порядке (3 задача)");
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            if (i > 0)
+                System.out.print(arr[i] + ", ");
+            else {
+                System.out.println(arr[i]);
+            }
+        }
+
+        for (int i = doubles.length - 1; i >= 0; i--) {
+            if (i > 0)
+                System.out.print(doubles[i] + ", ");
+            else {
+                System.out.println(doubles[i]);
+            }
+        }
+        for (int i = ints.length - 1; i >= 0; i--) {
+            if (i > 0)
+                System.out.print(ints[i] + ", ");
+            else {
+                System.out.println(ints[i]);
+            }
+        }
+System.out.println("Сделаем все числа четными (4 задача var.1)");
+
+        for (int i = 0; i < arr.length; i++) {
+            if ( i < arr.length-1 && arr[i] % 2 == 0) {
+                System.out.print(arr[i] + ", ");
+            }
+            else if ( i < arr.length-1 && arr[i] % 2 != 0) {
+                System.out.print((arr[i] + 1) + ", ");
+            }
+            else if (i == arr.length - 1 && arr[i] % 2 != 0) {
+                System.out.println(arr[i] + 1);
+            }
+            else if (i == arr.length - 1) {
+                System.out.println(arr[i]);
+            }
+        }
+
+System.out.println("Сделаем все числа четными (4 задача var.2)");
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i]%2 !=0) {
+                arr[i] = arr[i] + 1;
+            }
+            if (i < arr.length-1)
+                System.out.print(arr[i] + ", ");
+            else
+                System.out.println(arr[i]);
+        }
+
+
+System.out.println("PS. Вроде всё работает, но чувствуется мне, что это говнокод :)");
+
     }
 }
